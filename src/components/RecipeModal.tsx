9ece77224950b9a54d7926933a55df7791cf5ee7
to-biased <wagner.tobias.tw@gmail.com>
2022-11-07 22:29:05 +0100

    props definition

--- src/components/RecipeModal.tsx
+++ src/components/RecipeModal.tsx
@@ -1,9 +1,19 @@
+
 export const RecipeModal = () => {
     return (
         <>
-
-        <h1>Rezept</h1>
-
+            <div>
+                <h1>Recipe</h1>
+                    <p>Description</p>
+                <h2>Ingredients:</h2>
+                    <ul>
+                        <li>...</li>
+                    </ul>
+                <h2>Steps:</h2>
+                    <ul>
+                        <li>...</li>
+                    </ul>
+            </div>
         </>
     );
 };
