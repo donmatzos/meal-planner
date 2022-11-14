cca1eaeaada6f202a08bbf02c9b311229f6279d1
Bledixon Panda <12498599+Bledixon@users.noreply.github.com>
2022-11-14 20:18:02 +0100

    Adding new json recipes list

--- src/components/GenericCard.tsx
+++ src/components/GenericCard.tsx
@@ -17,21 +17,21 @@ export const GenericCard = ({description, index,  children}: CardProps) => {
         <div className="flex w-full max-h-96 shadow-md rounded-t-md">
             <div className="flex-col overflow-hidden rounded-md w-full bg-white m-2">
                 <img className="rounded-t-md" src="/sus.jpg"/>
-                <h2 className="font-bold">{data[index].Name}</h2>
+                <h2 className="font-bold">{data.recipe[index].name}</h2>
 
                 <p className="p-2 text-left text-ellipsis line-clamp-3 ..." >{description}</p>
 
                 <div className="z-20">
                     {/*<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={toggleModal}>Open Recipe</button>*/}
                     <GenericButtonModal
-                        title={data[index].Name}
+                        title={data.recipe[index].name}
                         isFab={false}
                         children={
                             <RecipeModal
-                                title={data[index].Name}
-                                description={data[index].Description || "-"}
-                                steps={data[index].Method}
-                                ingredients={data[index].Ingredients}
+                                title={data.recipe[index].name}
+                                description={data.recipe[index].description || "-"}
+                                /*steps={data.recipe[index].step}
+                                ingredients={data.recipe[index].Ingredients}*/
                             />}
                     />
                 </div>
