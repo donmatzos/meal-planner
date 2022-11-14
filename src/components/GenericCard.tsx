d431e5c045b2759cc2d423ac33fae13c90db9520
to-biased <wagner.tobias.tw@gmail.com>
2022-11-14 21:34:34 +0100

    format generic card

--- src/components/GenericCard.tsx
+++ src/components/GenericCard.tsx
@@ -13,15 +13,15 @@ export const GenericCard = ({description, index,  children}: CardProps) => {
     const toggleModal = () => setModalState(!isModalOpen);
 
     return(
-        <div className="flex w-full max-h-96 shadow-md rounded-t-md">
-            <div className="flex-col overflow-hidden rounded-md w-full bg-white m-2">
-                <img className="rounded-t-md overflow-hidden cover h" alt={data.recipe[index].name} src={data.recipe[index].image}/>
-                <h2 className="font-bold">{data.recipe[index].name}</h2>
+        <div className="flex w-full max-h-104 drop-shadow-lg rounded-t-md">
+            <div className="flex-col justify-between overflow-hidden rounded-md w-full bg-white m-2">
+                <img className="rounded-t-md object-cover w-full h-60" alt={data.recipe[index].name} src={data.recipe[index].image}/>
+                <h2 className="font-bold truncate">{data.recipe[index].name}</h2>
 
-                <p className="p-2 text-left text-ellipsis line-clamp-3 ..." >{description}</p>
+                <p className="p-2 text-left text-ellipsis line-clamp-2 h-16 ..." >{description}</p>
 
-                <div className="z-20">
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={toggleModal}>Open Recipe</button>
+                <div>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full m-4 bottom-0" onClick={toggleModal}>Open Recipe</button>
                     <div>
                         <RecipeModal
                             title={data.recipe[index].name}
