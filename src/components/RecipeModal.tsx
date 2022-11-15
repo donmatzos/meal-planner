bee43c55ae5c6470119d67cf08401a0ced20b756
to-biased <wagner.tobias.tw@gmail.com>
2022-11-15 22:03:44 +0100

    filling recipe, adjust style

--- src/components/RecipeModal.tsx
+++ src/components/RecipeModal.tsx
@@ -20,46 +20,43 @@ export const RecipeModal = ({title, description, isOpen, ingredients, steps, onC
     const dispatch = useAppDispatch();
     if (curRecipe.open) {
         return (
-            <div className={`flex fixed z-20 w-full min-h-screen top-0 left-0`}>
+            <div className={`flex fixed z-20 w-full text-white min-h-screen top-0 left-0`}>
 
-                <div className="flex flex-row w-full bg-white cursor-pointer shadow-md min-h-screen ">
+                <div className="flex flex-row w-full bg-zinc-900 shadow-md min-h-screen">
 
 
                     <img className="object-cover w-1/3 h-full" alt={data.recipe[curRecipe.index].name}
                          src={data.recipe[curRecipe.index].image}/>
 
                     <div className="flex-col text-left">
-
-                        <div className="m-1">
-                            <h1 className="text-4xl font-bold ml-10">{data.recipe[curRecipe.index].name}</h1>
-                            <p className="ml-11 text-xl">- {data.recipe[curRecipe.index].description}</p>
+                        <div className="">
+                            <h1 className="text-4xl font-bold font-oswald text-2xl p-6">{data.recipe[curRecipe.index].name}</h1>
+                            <h2 className="pl-8 pb-4">- {data.recipe[curRecipe.index].description}</h2>
                             <hr/>
                         </div>
 
-                        <div className="flex flex-row text-left overflow-auto ...">
-                            <div className="min-w-max pl-10 pt-5 ">
-                                <h2 className="text-xl font-bold">Ingredients:</h2>
-                                <br/>
-                                <ol className="space-y-0 list-disc list-inside text-gray-500 dark:text-gray">
+                        <div className="flex flex-row text-left overflow-auto...">
+                            <div className="min-w-max p-10 ">
+                                <h2 className="text-xl text-violet-400 font-bold pb-3">Ingredients:</h2>
+                                <ol className="space-y-0 list-disc list-inside ">
                                     {data.recipe[curRecipe.index].ingredient ? data.recipe[curRecipe.index].ingredient.map((ingred) =>
                                         <li>{ingred.amount} {ingred.unit} {ingred.name}</li>) : "-"}
                                 </ol>
                             </div>
-                            <div className="pt-5 pl-1 overflow-auto">
-                                <h2 className="text-xl font-bold">Steps:</h2>
-                                <br/>
-                                <ol className="space-y-0 list-disc text-gray-500 dark:text-gray ">
+                            <div className="p-10 overflow-auto">
+                                <h2 className="text-xl text-violet-400 font-bold pb-2">Steps:</h2>
+                                <ol className="space-y-0 list-disc">
                                     {data.recipe[curRecipe.index].step ? data.recipe[curRecipe.index].step.map((step) =>
                                         <li>{step.description}</li>) : "-"}
                                 </ol>
                             </div>
                         </div>
                     </div>
-                    <div className="fixed bg-white rounded-full m-4 hover:bg-gray">
-                        <button onClick={() => dispatch(toggleOpen(curRecipe))} >
-                            <img src={"../icons/close.svg"} alt="close recipe"/>
-                        </button>
-                    </div>
+                    <button className="fixed bg-zinc-900 hover:bg-violet-500 font-bold py-2 px-4 rounded-full m-4"
+                            onClick={() => dispatch(toggleOpen(curRecipe))}>
+                        X
+                    </button>
+
 
                 </div>
 
