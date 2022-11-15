ab011c803210a03ab613b75e3764f092b6d3258d
Bledixon Panda <12498599+Bledixon@users.noreply.github.com>
2022-11-15 23:20:41 +0100

    Adding day title

--- src/components/WeeklyPlanner.tsx
+++ src/components/WeeklyPlanner.tsx
@@ -3,16 +3,27 @@ import data from "../db/recipes.json";
 import { GenericCard } from "./GenericCard";
 
 export const WeeklyPlanner = () => {
-    const days = ["Monday", "Thuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    const days = [
+        "Monday",
+        "Thuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ];
 
     return (
         <div className="pl-14 pr-14 justify-center bg-slate-800 w-screen h-screen fixed">
             <div className="grid gap-x-96 grid-cols-7 items-center h-screen overflow-x-auto grid-rows-1">
-                    {days.map((x, i) => (
-                        <GenericCard key={i} index={-1}
-                        description={x}>
-           </GenericCard>
-                    ))}
+                {days.map((x, i) => (
+                    <div className="w-100">
+                        <GenericCard key={i} index={-1} description={x}>
+                            <></>
+                        </GenericCard>
+                        <h2 className="pt-16 w-100 text-center text-white font-oswald text-3xl font-medium">{x}</h2>
+                    </div>
+                ))}
             </div>
         </div>
     );
