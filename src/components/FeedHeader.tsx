a42cdbd557b094537881792b3d69bf00f3789eae
Bledixon Panda <12498599+Bledixon@users.noreply.github.com>
2022-11-15 21:23:59 +0100

    Restyling the main page

--- src/components/FeedHeader.tsx
+++ src/components/FeedHeader.tsx
@@ -8,16 +8,11 @@ export const FeedHeader = () => {
 
     return (
             <header className="App-header h-16">
-                <div className="absolute bg-white w-full h-16 drop-shadow-md top-0">
+                <div className="absolute bg-zinc-900 w-full h-16 drop-shadow-md top-0 font-oswald text-violet-400 font-medium">
                     <div className="flex h-full items-center">
-                        <img
-                            className="w-12 a"
-                            src={logo}
-                            alt="Meal Planner Logo"
-                        ></img>
-                        <h1 className="text-black text-left">Meal Planner</h1>
-                        <button onClick={() => dispatch(changeCategory(false))} className="hover:opacity-75 ml-4 pl-5 pr-5 text-center text-black text-3xl h-full shadow-[inset_1px_0px_4px_rgba(0,0,0,0.6)]">📅 Meal Planner</button>
-                        <button onClick={() => dispatch(changeCategory(true))} className="hover:opacity-75 pl-5 pr-5 text-center text-black text-3xl h-full shadow-[inset_-1px_0px_4px_rgba(0,0,0,0.6)]">🍔 Recipe List</button>
+                        <h1 className="text-left font-oswald">🍴Meal Planner</h1>
+                        <button onClick={() => dispatch(changeCategory(false))} className="hover:opacity-75 ml-4 pl-5 pr-5 text-center text-3xl h-full shadow-[inset_1px_0px_4px_rgba(0,0,0,0.6)]">📅 Meal Planner</button>
+                        <button onClick={() => dispatch(changeCategory(true))} className="hover:opacity-75 pl-5 pr-5 text-center text-3xl h-full shadow-[inset_-1px_0px_4px_rgba(0,0,0,0.6)]">🍔 Recipe List</button>
                     </div>
                 </div>
             </header>
--- src/components/GenericCard.tsx
+++ src/components/GenericCard.tsx
@@ -15,15 +15,15 @@ export const GenericCard = ({description, index,  children}: CardProps) => {
     const [isModalOpen, setModalState] = React.useState(false);
 
     return(
-        <div className="flex w-full max-h-104 drop-shadow-lg rounded-t-md">
-            <div className="flex-col justify-between overflow-hidden rounded-md w-full bg-white m-2">
+        <div className="flex w-full max-h-104 drop-shadow-lg rounded-t-md text-white">
+            <div className="flex-col justify-between overflow-hidden rounded-md w-full bg-zinc-900 m-2">
                 <img className="rounded-t-md object-cover w-full h-60" alt={data.recipe[index].name} src={data.recipe[index].image}/>
-                <h2 className="font-bold truncate">{data.recipe[index].name}</h2>
+                <h2 className="font-oswald text-2xl font-medium truncate">{data.recipe[index].name}</h2>
 
                 <p className="p-2 text-left text-ellipsis line-clamp-2 h-16 ..." >{description}</p>
 
                 <div>
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full m-4 bottom-0" onClick={() => dispatch(toggleOpen(index ?? 0))}>Open Recipe</button>
+                    <button className="bg-violet-400 hover:bg-violet-500 font-bold py-2 px-4 rounded-full m-4 bottom-0" onClick={() => dispatch(toggleOpen(index ?? 0))}>Open Recipe</button>
 
                 </div>
 
