bc6f30d721c60f39c609c46366787cd071fcd4bf
donmatzos <mgirkinger95@gmail.com>
2022-11-15 21:29:18 +0100

    refactored json, component adaptions

--- src/components/GenericButtonModal.tsx
+++ src/components/GenericButtonModal.tsx
@@ -27,7 +27,7 @@ export const GenericButtonModal = ({ title, isFab, isAddIcon, children }: Create
                         className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed
                         inset-0 z-50 outline-none focus:outline-none"
                     >
-                        <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                        <div className="relative w-4/5 my-6 mx-auto max-w-screen-2xl">
                             {/*content*/}
                             <div className="border-0 rounded-lg shadow-lg relative flex flex-col
                             w-full bg-white outline-none focus:outline-none">
