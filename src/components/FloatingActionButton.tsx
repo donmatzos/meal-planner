07d224316f25b4262db9eb46bfa06cdf74ea06ad
to-biased <wagner.tobias.tw@gmail.com>
2022-11-15 22:26:47 +0100

    update recipes.json

--- src/components/FloatingActionButton.tsx
+++ src/components/FloatingActionButton.tsx
@@ -8,7 +8,7 @@ export const FloatingActionButton = ({ isAddIcon, onClick }: FloatingActionButto
         <button onClick={onClick}
             className="fixed z-10 bottom-10 right-8 w-20 h-20
             rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl
-            bg-violet-400 hover:bg-violet-500"
+            bg-slate-100 hover:bg-violet-500"
         >
             { isAddIcon ? <span>&#10133;</span> : <span>&#128190;</span>}
         </button>
