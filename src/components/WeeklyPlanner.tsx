7c2bf052555ce5ef6875cd52a922d4da02f236ae
Bledixon Panda <12498599+Bledixon@users.noreply.github.com>
2022-11-16 21:33:33 +0100

    Update WeeklyPlanner.tsx

--- src/components/WeeklyPlanner.tsx
+++ src/components/WeeklyPlanner.tsx
@@ -1,17 +1,29 @@
 import React from "react";
 import data from "../db/recipes.json";
-import {GenericCard} from "./GenericCard";
+import { GenericCard } from "./GenericCard";
 
 export const WeeklyPlanner = () => {
-    const days = ["Monday", "Thuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    const days = [
+        "Monday",
+        "Thuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ];
 
     return (
-        <div className="justify-center bg-slate-800 w-screen h-screen fixed">
-            <div className="pt-10 grid gap-x-96 grid-cols-7 bg-white overflow-x-auto grid-rows-1">
+        <div className="pl-14 pr-14 justify-center bg-slate-800 w-screen h-screen fixed">
+            <div className="grid gap-x-96 grid-cols-7 items-center h-screen overflow-x-auto grid-rows-1">
                 {days.map((x, i) => (
-                    <GenericCard key={i} id=""
-                                 description={x}>
-                    </GenericCard>
+                    <div className="w-100">
+                        <GenericCard key={i} id={""} description={x}>
+                        </GenericCard>
+                        <h2 className="pt-16 w-100 text-center text-white font-oswald text-3xl font-medium">
+                            {x}
+                        </h2>
+                    </div>
                 ))}
             </div>
         </div>
