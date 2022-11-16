60ed23fe073e106a23d6e1613a0ef474ee8015a5
to-biased <wagner.tobias.tw@gmail.com>
2022-11-16 23:01:57 +0100

    restyle search

--- src/components/FeedHeader.tsx
+++ src/components/FeedHeader.tsx
@@ -14,7 +14,7 @@ export const FeedHeader = () => {
                         <h1 className="text-left font-oswald">🍴Meal Planner</h1>
                         <button onClick={() => dispatch(changeCategory(false))} className="hover:opacity-75 ml-4 pl-5 pr-5 text-center text-3xl h-full shadow-[inset_1px_0px_4px_rgba(0,0,0,0.6)]">📅 Meal Planner</button>
                         <button onClick={() => dispatch(changeCategory(true))} className="hover:opacity-75 pl-5 pr-5 text-center text-3xl h-full shadow-[inset_-1px_0px_4px_rgba(0,0,0,0.6)]">🍔 Recipe List</button>
-                        <input className="h-3/4 rounded-full bg-slate-800" placeholder="🔎 search...."type="text" onChange={(e) => dispatch(search(e.target.value))} />
+                        <input className="px-4 ml-2 h-2/3 text-lg rounded-full bg-slate-800" placeholder="🔎 search for recipe..." type="text" onChange={(e) => dispatch(search(e.target.value))} />
                     </div>
                 </div>
             </header>
